b97bdaa00d000a40a568916b93bfdb3a8e447f10
yiendos <yiendos@sbmatrix.com>
2014-03-12 18:26:06 +0000

    re inclusion of google tracking code

--- application/site/public/theme/bootstrap/templates/application/page/default.html.php
+++ application/site/public/theme/bootstrap/templates/application/page/default.html.php
@@ -102,5 +102,15 @@
 <script src="/theme/bootstrap/js/bootstrap.js"></script>
 <?//@todo remove cdn and deliver locally ?>
 <style src="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" />
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-48921009-1', 'snowsportforsheffield.co.uk');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
